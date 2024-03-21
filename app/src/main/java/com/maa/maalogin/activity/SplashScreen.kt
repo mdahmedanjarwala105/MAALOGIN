83ef5957f83454ec79102ffd8535510418c0644f
Mohammed Ahmed Anjarwala <mdahmedanjarwala105@gmail.com>
2024-03-21 18:05:20 +0530

    Splash Screen Update

---
+++ app/src/main/java/com/maa/maalogin/activity/SplashScreen.kt
@@ -0,0 +1,34 @@
+package com.maa.maalogin.activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.maa.maalogin.R
+
+class SplashScreen : AppCompatActivity() {
+
+    private val SPLASH_TIME: Long = 3000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash_screen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.progressLayout)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this@SplashScreen, LoginActivity::class.java))
+        }, SPLASH_TIME)
+    }
+    override fun onPause() {
+        super.onPause()
+        finish()
+    }
+}
